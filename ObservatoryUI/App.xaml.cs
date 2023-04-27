a2a9c60093e20060c468c1e81939e1a860d419ee
Paul Grimstrup <paul.grimstrup@gmail.com>
2023-04-27 15:01:16 +1200

    WIP (does not compile)! Added .NET MAUI user interface project.

---
+++ ObservatoryUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ObservatoryUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ObservatoryUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ObservatoryUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
